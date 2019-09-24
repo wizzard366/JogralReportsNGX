3e00d4c47ce913f406c574711f25db20dca579a4
wizzard366 <wizzard366@outlook.com>
2019-09-23 22:12:19 -0600

    fixing total on product detail, adding average

--- ng2-admin/src/app/pages/precios/components/salesGraph/sales-graph.component.ts
+++ ng2-admin/src/app/pages/precios/components/salesGraph/sales-graph.component.ts
@@ -60,6 +60,8 @@ export class SalesGraphComponent {
     let currentYear=0;
     let temp_money_total=0;
     let temp_qty_total=0;
+    let temp_month_average_qty=0;
+    let temp_month_average_money=0;
 
     let graphDataMoney=[];
     let graphDataQuantity=[]
@@ -84,13 +86,17 @@ export class SalesGraphComponent {
         tempTableQuantityDataRow["Tipo"]="Cantidad Unidades";
         tempTableQuantityDataRow[this.keys[element.Mes-1]]=element.Cantidad.toLocaleString('en-US');
         temp_qty_total = temp_qty_total + element.Cantidad;
+        temp_month_average_qty = temp_qty_total/element.Mes;
         tempTableQuantityDataRow['Total'] = temp_qty_total.toLocaleString('en-US');
+        tempTableQuantityDataRow['MAverage'] = temp_month_average_qty.toLocaleString('en-US');
         tempTableMoneyDataRow["Ano"]=currentYear;
         tempTableMoneyDataRow["class"]=chartistColorClasses[class_index];
         tempTableMoneyDataRow["Tipo"]="Monto Quetzalez";
         tempTableMoneyDataRow[this.keys[element.Mes-1]]=element.Monto.toLocaleString('en-US');
         temp_money_total = temp_money_total + element.Monto;
+        temp_month_average_money = temp_money_total/element.Mes;
         tempTableMoneyDataRow['Total'] = temp_money_total.toLocaleString('en-US');
+        tempTableMoneyDataRow['MAverage'] = temp_month_average_money.toLocaleString('en-US');
         tempGraphDataMoneyRow.push(element.Monto);
         tempGraphDataQuantityRow.push(element.Cantidad);
         class_index++;
@@ -115,13 +121,17 @@ export class SalesGraphComponent {
         tempTableQuantityDataRow["Tipo"]="Cantidad Unidades";
         tempTableQuantityDataRow[this.keys[element.Mes-1]]=element.Cantidad.toLocaleString('en-US');
         temp_qty_total = temp_qty_total + element.Cantidad;
+        temp_month_average_qty = temp_qty_total/element.Mes;
         tempTableQuantityDataRow['Total'] = temp_qty_total.toLocaleString('en-US');
+        tempTableQuantityDataRow['MAverage'] = temp_month_average_qty.toLocaleString('en-US');
         tempTableMoneyDataRow["Ano"]=currentYear;
         tempTableMoneyDataRow["class"]=chartistColorClasses[class_index];
         tempTableMoneyDataRow["Tipo"]="Monto Quetzalez";
         tempTableMoneyDataRow[this.keys[element.Mes-1]]=element.Monto.toLocaleString('en-US');
         temp_money_total = temp_money_total + element.Monto;
+        temp_month_average_money = temp_money_total/element.Mes;
         tempTableMoneyDataRow['Total'] = temp_money_total.toLocaleString('en-US');
+        tempTableMoneyDataRow['MAverage'] = temp_month_average_money.toLocaleString('en-US');
         tempGraphDataMoneyRow.push(element.Monto);
         tempGraphDataQuantityRow.push(element.Cantidad);
         class_index++;
@@ -129,9 +139,13 @@ export class SalesGraphComponent {
       }else{
         tempTableQuantityDataRow[this.keys[element.Mes-1]]=element.Cantidad.toLocaleString('en-US');
         temp_qty_total = temp_qty_total + element.Cantidad;
+        temp_month_average_qty = temp_qty_total/element.Mes;
         tempTableQuantityDataRow['Total'] = temp_qty_total.toLocaleString('en-US');
+        tempTableQuantityDataRow['MAverage'] = temp_month_average_qty.toLocaleString('en-US');
         tempTableMoneyDataRow[this.keys[element.Mes-1]]=element.Monto.toLocaleString('en-US');
         temp_money_total = temp_money_total + element.Monto;
+        temp_month_average_money = temp_money_total/element.Mes;
+        tempTableMoneyDataRow['MAverage'] = temp_month_average_money.toLocaleString('en-US');
         tempTableMoneyDataRow['Total'] = temp_money_total.toLocaleString('en-US');
         tempGraphDataMoneyRow.push(element.Monto);
         tempGraphDataQuantityRow.push(element.Cantidad);
